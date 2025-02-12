99f42cb0ae003db8e8b7d00b0d5695c6a9f58fc0
TheDavidon <vdavidonka@bk.ru>
2025-02-12 13:57:18 +0300

    feat: add solution interpreter class and empirical values for consts

--- logistics_tasks/discrete_cargo_single_agent_QUBO.py
+++ logistics_tasks/discrete_cargo_single_agent_QUBO.py
@@ -33,6 +33,11 @@ class QUBOData:
     QUBO_dict: defaultdict[tuple[str, str], int]
     offset: int
 
+@dataclasses.dataclass
+class SolutionData:
+    path: list[int]
+    cargo_actions: list[tuple[int, int]]
+
 class HamiltonianInitializer:
     def __init__(self, formatted_data):
         self.data: FormattedData = formatted_data
@@ -55,13 +60,13 @@ class HamiltonianInitializer:
         self.load_num = [self.auxiliary_variables_to_number_expression(i) for i in range(self.data.q)]
         self.cargo_status = [[sum([self.cargo_take_variables[j][k] - self.cargo_give_variables[j][k] for k in range(i + 1)]) for j in range(self.data.cargo_count)] for i in range(self.data.q)]
     def initialize_consts(self):
-        self.const_correct_end_points = 1
-        self.const_correct_path = 1
+        self.const_correct_end_points = 10
+        self.const_correct_path = 100
         self.const_correct_loads = 1
-        self.const_correct_take_give_action = 1
-        self.const_correct_take_vertices = 1
-        self.const_correct_give_vertices = 1
-        self.const_correct_order = 1
+        self.const_correct_take_give_action = 50
+        self.const_correct_take_vertices = 100
+        self.const_correct_give_vertices = 100
+        self.const_correct_order = 200
         self.const_path_length = 1
 
     def initialize_hamiltonians(self):
@@ -111,5 +116,67 @@ class QUBOInitializer:
                 raise ValueError
         return QUBOData(Q, offset)
 
+
+class QUBOSolutionInterpreter:
+    def __init__(self, variable_value, formatted_data):
+        self.variable_value = variable_value
+        self.formatted_data = formatted_data
+    def get_solution(self):
+        self.validate_solution()
+        return SolutionData(path=self.path, cargo_actions=self.cargo_actions)
+    def validate_solution(self):
+        self.validate_path()
+        self.validate_cargo()
+        self.validate_load()
+
+    def validate_path(self):
+        self.path = []
+        for i in range(self.formatted_data.q):
+            visited_vertices = []
+            for j in range(self.formatted_data.vertex_count):
+                if self.variable_value[f"x_{i}_{j}"]:
+                    visited_vertices.append(j)
+            if len(visited_vertices) != 1:
+                print(f"Not single vertex at {i} turn : ", *visited_vertices)
+                self.path.append(tuple(visited_vertices))
+            else:
+                self.path.append(visited_vertices[0])
+    def validate_cargo(self):
+        self.cargo_actions = []
+        for i in range(self.formatted_data.cargo_count):
+            start_turn, finish_turn = [], []
+            for j in range(self.formatted_data.q):
+                if self.variable_value[f"a_{i}_{j}"]:
+                    start_turn.append(j)
+                if self.variable_value[f"b_{i}_{j}"]:
+                    finish_turn.append(j)
+            if len(start_turn) != 1:
+                print(f"Not single turn for {i} cargo take: ", *start_turn)
+                start_turn = tuple(start_turn)
+            else:
+                start_turn = start_turn[0]
+                if self.path[start_turn] != self.formatted_data.cargo_list[i].start_vertex:
+                    print(f"Cargo {i} taken at wrong vertex {self.path[start_turn]} instead of {self.formatted_data.cargo_list[i].start_vertex}")
+            if len(finish_turn) != 1:
+                print(f"Not single turn for {i} cargo give: ", *finish_turn)
+                finish_turn = tuple(finish_turn)
+            else:
+                finish_turn = finish_turn[0]
+                if self.path[finish_turn] != self.formatted_data.cargo_list[i].finish_vertex:
+                    print(f"Cargo {i} given at wrong vertex {self.path[finish_turn]} instead of {self.formatted_data.cargo_list[i].finish_vertex}")
+            self.cargo_actions.append((start_turn, finish_turn))
+            
+            
+    def validate_load(self):
+        actual_load = 0
+        for i in range(self.formatted_data.q):
+            for j in range(self.formatted_data.cargo_count):
+                if self.variable_value[f"a_{j}_{i}"]:
+                    actual_load += self.formatted_data.cargo_list[j].weight
+                if self.variable_value[f"b_{j}_{i}"]:
+                    actual_load -= self.formatted_data.cargo_list[j].weight
+            auxiliary_variable_load = sum([self.variable_value[f"y_{i}_{k}"] * 2 ** k for k in range(self.formatted_data.M)]) + (self.formatted_data.capacity + 1 - 2 ** self.formatted_data.M) * self.variable_value[f"y_{i}_{self.formatted_data.M}"]
+            if actual_load != auxiliary_variable_load:
+                print(f"At turn {i} actual load not equal to auxiliary load: {actual_load} != {auxiliary_variable_load}")
 
 
